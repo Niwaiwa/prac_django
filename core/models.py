99e3d2beee0cde39e213d636f5ec3b9208f3a443
Niwaiwa <po2846@gmail.com>
2023-04-13 23:45:30 +0800

    change category model to item app

--- core/models.py
+++ core/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
