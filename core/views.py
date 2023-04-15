eb9ce8918259439628479cd7e4c34716d47efcf3
Niwaiwa <po2846@gmail.com>
2023-04-15 23:04:02 +0800

    add signup api and html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignUpForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -10,6 +12,17 @@ def index(request):
         'items': items
     })
 
-def contract(request):
-    return render(request, 'core/contract.html')
+def contact(request):
+    return render(request, 'core/contact.html')
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
